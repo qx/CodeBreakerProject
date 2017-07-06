0a0c5c7e8c59eeabbe23cf10e4be0bc0445477a6
ok <ok@okdeiMac.local>
2017-07-06 09:01:47 +0800

    set message

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,6 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
-
+let mmessage = document.getElementById('message');
 function guess() {
   let input = document.getElementById('user-guess');
   //add functionality to guess function here
@@ -15,6 +15,11 @@ function setHiddenFields(){
   }
   answer.value = num
 
+}
+
+function setMessage(mmessage){
+  mmessage.innerHTML = "kkkkkk";
+  //  alert("kkjkj");
 }
 
 
